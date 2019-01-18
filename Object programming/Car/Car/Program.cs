7864ed5afae6033731bc05415d9b85d9e51fa311
eero94 <eero.oittinen@gmail.com>
2019-01-18 11:14:12 +0200

    Car Program Done

---
+++ Object programming/Car/Car/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Autot");
+
+            Car car = new Car();
+            car.AskData();
+            car.ShowInfo();
+            Console.WriteLine($"Anna km/h arvo jolla  nopeutta lisätään.");
+            car.Accelerate(Console.ReadLine());
+            car.Brake();
+            Console.WriteLine("\n------------\n");
+
+            Car Secondcar = new Car();
+            car.AskData();
+            car.ShowInfo();
+            Console.WriteLine($"Anna km/h arvo jolla  nopeutta lisätään.");
+            car.Accelerate(Console.ReadLine());
+            car.Brake();
+            Console.WriteLine("\n------------\n");
+
+
+
+
+        }
+    }
+}
