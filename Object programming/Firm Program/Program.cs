a779b55336d41b94b1bbc6aa7a0b8d9ae7e55b9e
eero94 <eero.oittinen@gmail.com>
2019-01-23 13:03:34 +0200

    Firm program done

---
+++ Object programming/Firm Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace firm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Firm");
+
+            firm data = new firm();
+            data.AskData();
+
+            data.Profitmargin();
+        }
+    }
+}
