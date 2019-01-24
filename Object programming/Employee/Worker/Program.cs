e4369b118cc016f9bc1bdbde4f17c437aa283841
eero94 <eero.oittinen@gmail.com>
2019-01-24 10:27:11 +0200

    Employee program done

---
+++ Object programming/Employee/Worker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employees:");
+            Console.WriteLine("---------------------------------");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("a", "siivoja", 1000);
+            employees[1] = new Employee("b", "huoltomies", 1250);
+            employees[2] = new Employee("c", "korjaaja", 1500);
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine(employee.ToString());
+            }
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine(Employee.CompareSalary(employees[0], employees[1]));
+            Console.WriteLine(Employee.CompareSalary(employees[1], employees[2]));
+            Console.WriteLine(Employee.CompareSalary(employees[0], employees[2]));
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+        } 
+    }
+}
---
+++ Object programming/Employee/Worker/worker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worker
+{
+    
+    class Employee
+    {
+        public string name;
+        public string position;
+        public int salary = 1;
+
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name,  string position, int salary)
+        {
+            this.name = name;           
+            this.position = position;
+            this.salary = salary;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Nimi: {this.name}\n" +
+                             $"Tehtävä: {this.position}\n" +
+                             $"Palkka: {this.salary.ToString()}\n");
+
+        }
+
+        public override string ToString()
+        {
+            return $"Employee: {name}\nposition {position}\nSalary:{salary:c}\n";
+        }
+
+
+
+        public static string CompareSalary(Employee firstEmployee, Employee secondEmployee)
+        {
+            if (firstEmployee.salary > secondEmployee.salary)
+            {
+                return $"{firstEmployee.name} has a higher salary than {secondEmployee}";
+            }
+            else if (firstEmployee.salary < secondEmployee.salary)
+            {
+                return $"{secondEmployee.name} has a higher salary than {firstEmployee.name}";
+            }
+            else
+            {
+                return $"{firstEmployee} and { secondEmployee} have an equal salary.";
+            }
+        }
+    }
+}
