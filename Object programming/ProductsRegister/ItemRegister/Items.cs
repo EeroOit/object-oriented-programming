3d5e87ffaf4787cfcfc13f30085bcb9f6b9170c8
eero94 <eero.oittinen@gmail.com>
2019-03-10 20:46:32 +0200

    Products Register program done.

---
+++ Object programming/ProductsRegister/ItemRegister/Items.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace ProductsRegister
+{
+    class ItemsList
+
+
+
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string groupName { get; set; }
+        public double price { get; set; }
+        public int amount { get; set; }
+        public string comment { get; set; }
+    }
+}
